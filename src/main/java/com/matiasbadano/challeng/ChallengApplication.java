b5ce7576c6e6a7f41723c40716ec4c35b4620bbf
Matias Badano <matiasss95@gmail.com>
2023-07-06 17:26:45 -0300

    Primer commit

---
+++ src/main/java/com/matiasbadano/challeng/ChallengApplication.java
@@ -0,0 +1,13 @@
+package com.matiasbadano.challeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matiasbadano/challeng/ChallengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiasbadano.challeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
