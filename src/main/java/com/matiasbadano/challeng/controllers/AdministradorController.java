8aff8303b10fb599a0863f71b10e494be12a06c1
Matias Badano <matiasss95@gmail.com>
2023-07-11 11:48:50 -0300

    Update AdministradorController.java

--- src/main/java/com/matiasbadano/challeng/controllers/AdministradorController.java
+++ src/main/java/com/matiasbadano/challeng/controllers/AdministradorController.java
@@ -122,7 +122,7 @@ public class AdministradorController {
         curso.setNombre(nombre);
 
         Categoria categoria = new Categoria();
-        categoria.setId(categoriaId);
+        categoria.setId((long) categoriaId);
         curso.setCategoria(categoria);
 
         curso.setTurno(turno);
